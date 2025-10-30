9e737a409874c656a731b16e0e8215ba0657ea6b
Ratha <rathaphearom222@gmail.com>
2025-10-30 11:30:29 +0700

    refactor: update navbar and product links for better navigation; remove test.html and add detailProduct.html

--- js/FetchJson.js
+++ js/FetchJson.js
@@ -7,13 +7,13 @@ fetch("Product.json")
 .then(data=>{
     data.Products.forEach((value=>{
         result += `
-         <div class="col-lg-3 mt-4"  key=${value.id}>
+         <div class="col-lg-3 mt-4" onclick=" window.location.href='/Page/detailProduct.html?id=${value.id}'">
           <div class="card">
                     <img src="${value.image}" alt="${value.name}" class="card-img-top">
                     <div class="card-body">
                       <h3 class="card-title">${value.Price}$  </h3>
                       <p class="card-text">${value.title}</p>
-                      <a href="${value.link}" class="btn btn-primary">BUY NOW</a>
+                      <a href="/Page/detailProduct.html?id=${value.id}" class="btn btn-primary">BUY NOW</a>
                     </div>
             </div>
           </div>
@@ -23,8 +23,8 @@ fetch("Product.json")
     // getclothes
     data.Clothes.forEach((value=>{
       resultClothes +=`
-      <div class="col-lg-3 mt-4"  key=${value.id}>
-          <div class="card">
+      <div class="col-lg-3 mt-4"  >
+          <div class="card"  onclick=" window.location.href='/Page/detailProduct.html?id=${value.id}'">
                     <img src="${value.image}" alt="${value.name}" class="card-img-top">
                     <div class="card-body">
                       <h3 class="card-title">${value.price}$  </h3>
